71bf96bdacec4e884a29df368cf1743384016216
Ebenezer Isaac <mail@ebenezer-isaac.com>
2024-06-13 23:10:06 +0530

    minor updates for redeployemnt

--- logger.php
+++ logger.php
@@ -97,7 +97,7 @@ try {
     $city = $json['city'];
     $zip = $json['zip'];
     $isp = $json['isp'];
-    $servername = "sql290.main-hosting.eu";
+    $servername = "srv677.hstgr.io";
     $username = "u117204720_analytics";
     $password = "ex46Z>n?";
     $dbname = "u117204720_analytics";
